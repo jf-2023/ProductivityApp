1a7c7e919eabbe4305806f12b1026c6d4fe0f5ae
Juan <mr.muffin235@gmail.com>
2023-06-16 00:52:51 -0500

    update commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 function Main(){
-    return (
-    <div className="Todo">
+    const [tasks, setTasks] = useState([])
+    const [text, setText] = useState("")
 
+    function getText(e) {
+        setText(e.target.value);
+    }
+
+    function onSubmit(event){
+        event.preventDefault();
+        if (text.trim() !== '') {
+            setTasks([...tasks, text])
+            setText('')
+        }
+    }
+
+
+    return (
+    <div className="Todo-Container">
+        <h1>Daily Tasks</h1>
+        <form action="" className="Todo">
+            <input type="text" value={text} className="add-todo" onChange={getText}/>
+            <button className="add-button" onClick={onSubmit}>Add Task</button>
+        </form>
+            {tasks.map((task, key) => 
+            (<div className='task-container' key={key}>
+                {task}
+                <button >Complete!</button>
+                <button>Delete</button>
+            </div>))}
     </div>
 )}
 
