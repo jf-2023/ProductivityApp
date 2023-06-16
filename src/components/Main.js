fcdc6f6adba96c656fdedd6aabe9369360c1bcd9
Juan <mr.muffin235@gmail.com>
2023-06-16 00:44:15 -0500

    init

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Main(){
+    return (
+    <div className="Todo">
+
+    </div>
+)}
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav(){
+    return(
+    <nav className="Navbar">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtZjoiIG-xTqbiIiFcRHnssZ2cUF0MagahpT2wd30lgA&usqp=CAU&ec=48665699.jpg" alt="sorry :(" className='nav-logo'/>
+        <h1>SlugHub</h1>
+        <h3>Project 1</h3>
+    </nav>
+)}
+
+export default Nav
