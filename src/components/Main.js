669e2aaaf0887748bf0fbaf5e86e3922056ad957
Juan <mr.muffin235@gmail.com>
2023-06-18 02:53:41 -0500

    added delete button functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,9 @@ import React from "react";
 import { useState } from "react";
 
 function Main(){
-    const [tasks, setTasks] = useState([])
-    const [text, setText] = useState("")
+    const [tasks, setTasks] = useState([]);
+    const [text, setText] = useState("");
+    const [count, setCount] = useState("");
 
     function getText(e) {
         setText(e.target.value);
@@ -17,6 +18,11 @@ function Main(){
         }
     }
 
+    function handleDelete(key){
+        const newTasks = [...tasks];
+        newTasks.splice(key, 1);
+        setTasks(newTasks)
+    }
 
     return (
     <div className="Todo-Container">
@@ -27,9 +33,9 @@ function Main(){
         </form>
             {tasks.map((task, key) => 
             (<div className='task-container' key={key}>
-                {task}
-                <button >Complete!</button>
-                <button onClick={() => handleDelete(key)}>Delete</button>
+                <span style={{ textDecoration: count % 2 == 0 ? 'line-through' : '' }}>{task}</span>
+                <button onClick={() => setCount(count + 1)}>Complete!</button>
+                <button onClick={() => handleDelete({key})}>Delete</button>
             </div>))}
     </div>
 )}
