a4f81822d9f210991673259b450ca9b811a95cfe
Juan <mr.muffin235@gmail.com>
2023-06-24 13:46:55 -0500

    new

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import './App.css';
 import Nav from './components/Navbar';
 import Main from './components/Main';
 import Timer from './components/Timer';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+ const [newDay, setNewDay] = useState('12:00:00 AM')
+
+useEffect(() => {
+  const timer = setInterval(() =>{
+      const currentTime = new Date().toLocaleTimeString()
+      if (currentTime == newDay){
+        handleResetTasks();
+      }
+  }, 1000);
+
+  return () => {
+    clearInterval(timer);
+  };
+
+}, [newDay]);
+
+const handleResetTasks = () => {
+  const updatedTasks = tasks.map((task) => {
+    return { ...task, complete: false };
+  });
+};
+
   return (
     <div className="App">
       <Nav />
