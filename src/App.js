2d5c38adbd1182243e8efc977a91aa05bf795848
Juan <mr.muffin235@gmail.com>
2023-08-03 11:26:05 -0700

    added practice component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import Expenses from './components/Expenses';
-import Goals from './components/Goals';
-import Pomodoro from './components/Pomodoro';
-import Stones from './components/Habits';
-import Todo2 from './components/Todo-list';
-import React, { useState } from 'react';
+import Practice from './components/Practice';
+import React from 'react';
 
 function App() {
   return (
-    <div className="container">
-      <div className="item item-1"> <Todo2 /> </div>
-      <div className="item item-2"> <Goals /> </div>
-      <div className="item item-3"> <Expenses /> </div>
-      <div className="item item-4"> <Stones /> </div>
-      <div className="item item-5"> <Pomodoro /> </div>
-    </div>
+    <Practice />
   );
 }
 
---
+++ src/components/Practice.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Practice(){
+    return(
+        <div>
+            <h1 class='test'>Hello World</h1>
+        </div>
+    )
+}
